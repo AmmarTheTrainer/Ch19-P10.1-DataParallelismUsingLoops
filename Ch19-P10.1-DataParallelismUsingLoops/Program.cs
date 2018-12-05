8607b2a09741fe173ceb6de4078b2256f7238f5d
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-12-05 14:38:27 +0500

    Project Created

---
+++ Ch19-P10.1-DataParallelismUsingLoops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch19_P10._1_DataParallelismUsingLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
